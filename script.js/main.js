f43cd5381014ff5b488b247a5cb1343221f569e3
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-28 15:42:41 +0100

    aggiunto imageArray

--- script.js/main.js
+++ script.js/main.js
@@ -5,3 +5,17 @@ const imageArray = [
     'img/04.webp',
     'img/05.webp',
 ]
+
+let caroselContent = ""
+
+const caroselDom = document.querySelector('.carosel');
+for(let i = 0; i < imageArray.length; i++) {
+    const newImageWrapper = ` <div class="image-wrapper">
+                    <img class="image" src="${imageArray[i]}" alt="Prima immagine" />
+                </div>`;
+
+                caroselContent += newImageWrapper;
+
+}
+
+contentDom.innerHTML = caroselContent;
